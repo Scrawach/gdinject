078433dfa0b257620ea5eec0b4610601aa54f8fc
scrawach <scrawach@gmail.com>
2024-08-05 01:37:22 +0300

    a little bit test name refactoring

--- tests/test_di_container.gd
+++ tests/test_di_container.gd
@@ -1,6 +1,6 @@
 extends GutTest
 
-func test_when_resolve_by_contract_then_should_return_instance_binded_with_this_contract() -> void:
+func test_resolve_instance_by_contract() -> void:
 	# arrange
 	var builder := DIBuilder.new()
 	builder.bind(ILogger).to(ConsoleLogger).as_single()
