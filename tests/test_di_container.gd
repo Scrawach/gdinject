9d54b9b9b9facefc67838b0dc8c385dcad7bbd45
scrawach <scrawach@gmail.com>
2024-08-05 03:19:16 +0300

    added test for resolve dependency with arguments in constructor

--- tests/test_di_container.gd
+++ tests/test_di_container.gd
@@ -12,3 +12,17 @@ func test_resolve_instance_by_contract() -> void:
 	# assert
 	assert_not_null(logger)
 	assert_true(logger is ConsoleLogger)
+
+func test_resolve_instance_by_contract_with_dependencies() -> void:
+	# arrange
+	var builder := DIBuilder.new()
+	builder.bind(ILogger).to(ConsoleLogger).as_single()
+	builder.bind(ICommand).to(DefaultCommand).as_single()
+	var container: DIContainer = builder.build()
+	
+	# act
+	var command = container.resolve(ICommand)
+	
+	# assert
+	assert_not_null(command)
+	assert_true(command is DefaultCommand)
