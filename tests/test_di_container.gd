435be80554f8249235064ab0ef5020e3a638835a
scrawach <scrawach@gmail.com>
2024-08-06 16:38:21 +0300

    removed unavailable asserts with null for dynamic scripts

--- tests/test_di_container.gd
+++ tests/test_di_container.gd
@@ -10,7 +10,6 @@ func test_resolve_instance_by_contract() -> void:
 	var logger = container.resolve(ILogger)
 	
 	# assert
-	assert_not_null(logger)
 	assert_true(logger is ConsoleLogger)
 
 func test_resolve_instance_by_contract_with_dependencies() -> void:
@@ -24,5 +23,4 @@ func test_resolve_instance_by_contract_with_dependencies() -> void:
 	var command = container.resolve(ICommand)
 	
 	# assert
-	assert_not_null(command)
 	assert_true(command is DefaultCommand)
