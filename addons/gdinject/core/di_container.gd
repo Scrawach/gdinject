2d67051613ebcf6454042c5a5c32a69308f2f593
scrawach <scrawach@gmail.com>
2024-08-08 18:59:10 +0300

    propogate request for resolve di container to service locator

--- addons/gdinject/core/di_container.gd
+++ addons/gdinject/core/di_container.gd
@@ -7,8 +7,4 @@ func _init(services: DIServiceLocator) -> void:
 	self.services = services
 
 func resolve(key) -> Variant:
-	if services.has(key):
-		return services.find(key)
-	
-	push_error("DIContainer: can't resolve key %s" % key)
-	return null
+	return services.find(key)
