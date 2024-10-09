0d1adc8c0c5ace46280e7a05d518e4cc7db7a6d6
scrawach <scrawach@gmail.com>
2024-10-09 16:15:25 +0300

    removed commented code from test node

--- addons/gdinject/examples/test.gd
+++ addons/gdinject/examples/test.gd
@@ -1,11 +1,6 @@
 extends Node
 
 func _ready() -> void:
-	#var callback = DefaultCommand.new.bind("my_tag").bind(logger)
-	#var callback = DefaultCommand.new.bindv([logger, "my_tag"])
-	#var value = callback.call() as DefaultCommand
-	#value.execute()
-	#return
 	var builder := DIBuilder.new()
 	builder.bind(ILogger).to(ConsoleLogger).as_single()
 	builder.bind(ICommand).to(DefaultCommand).as_single()
